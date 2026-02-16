bdcad02f0a5110ea712cd7e54238f8330ca93dc5
y.nitta <yosshi3800@gmail.com>
2026-02-17 07:41:10 +0900

    弾を実装しました。

---
+++ WaveSurvival/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _lifeTime = 3f;
+
+    private Vector2 _direction;
+    private float _timer;
+
+    public void Init(Vector2 dir)
+    {
+        _direction = dir.normalized;
+        _timer = _lifeTime;
+    }
+
+    void Update()
+    {
+        transform.Translate(_direction * _speed * Time.deltaTime);
+
+        _timer -= Time.deltaTime;
+        if (_timer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter2D");
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy");
+            Destroy(gameObject);
+        }
+    }
+}
--- WaveSurvival/Assets/Scripts/Player/PlayerController.cs
+++ WaveSurvival/Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _fireCooldown = 0.3f;
+    private float _fireTimer;
     private Rigidbody2D _rb;
     private Vector2 _input;
 
@@ -14,9 +18,28 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     { 
+        //ˆÚ“®ˆ—
         _input.x = Input.GetAxisRaw("Horizontal");
         _input.y = Input.GetAxisRaw("Vertical");
         _input = _input.normalized;
         _rb.linearVelocity = _input * _moveSpeed;
+
+        //’e”­Ëˆ—
+        _fireTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _fireTimer <= 0f)
+        {
+            Fire();
+            _fireTimer = _fireCooldown;
+        }
     }
+
+    //’e”­Ëˆ—
+    private void Fire()
+    {
+        GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.identity);
+        Vector2 dir = Vector2.right; 
+        bullet.GetComponent<Bullet>().Init(dir);
+    }
+
 }
